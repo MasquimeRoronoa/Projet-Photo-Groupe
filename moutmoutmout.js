65bc2b88b583839882d4933c4649e482c6434ebe
maxence.schepens <maxence.schepens@gmail.com>
2021-02-15 09:15:18 +0100

    Vasy franky

--- moutmoutmout.js
+++ moutmoutmout.js
@@ -4,6 +4,20 @@ $(window).ready(function () {
     var userlogin = $('#login').val()
     var usermdp = $('#mdp').val()
     var private = $('#private')
+    var userObj
+    function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    if (!localStorage.getitem('accounts')) {
+        userObj = {
+            "users": []
+        }
+    } else {
+        userObj = JSON.parse(localStorage.getItem('accounts'))
+    }
     private.hide()
     $("#connecting").submit(function (event) {
         event.preventDefault()
@@ -15,13 +29,45 @@ $(window).ready(function () {
         }
 
     })
-    $('#inscription').submit(function(event){
+    $('#inscription').submit(function (event) {
         event.preventDefault()
         var password = $('#passworde').val()
         var pseudo = $('#pseudo').val()
         if (password == "" || pseudo == "") {
             alert("Champs pas remplis")
-        }
+        } else {
+            // le pseudo existe il ? et si oui le mdp correspond il ?
+            let pseudoExist = false
+            let x
+            for (x in userObj.users) {
+                let actualUser  = userObj.users[x]
+                if (actualUser.pseudo == pseudo) {
+                    pseudoExist = true
+                    break;
+                }
+            }
+            if (pseudoExist) {
+                alert(Pseudo déjà existant)
+            } else {
+                //Etape : créateur de l'objet utilisateur :
+                var user = {
+                    id: uuidv4(),
+                    pseudo: pseudo,
+                    mdp: mdp,
+                }
+                //Etape d : Ajout du nouvel utilisateur dans la liste des utilisateurs
+                usersObj.users.push(user);
+                //Etape e : Sauvegarde dans le localStorage
+                saveUsers()
+
+                //ETAPES FACULTATIVES :
+                //On va vider les champs :
+                $('#registerPseudo').val("")
+                $('#registerPassword').val("")
+                //on va afficher le panneau de login
+                displayLogin()
+            }
+                }
 
     })
 })
