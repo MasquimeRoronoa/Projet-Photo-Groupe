df08782cdbaf90ac14f7c126898e4879584f9b27
maxence.schepens <maxence.schepens@gmail.com>
2021-02-17 08:47:29 +0100

    Oui bonjour

--- moutmoutmout.js
+++ moutmoutmout.js
@@ -1,73 +1,161 @@
-$(window).ready(function () {
-    localStorage.getItem('Utilisateurs')
-    var MyJson;
-    var userlogin = $('#login').val()
-    var usermdp = $('#mdp').val()
-    var private = $('#private')
-    var userObj
-    function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    }
-    if (!localStorage.getitem('accounts')) {
-        userObj = {
-            "users": []
-        }
-    } else {
-        userObj = JSON.parse(localStorage.getItem('accounts'))
-    }
-    private.hide()
-    $("#connecting").submit(function (event) {
-        event.preventDefault()
-        if (usermdp.length < 6) {
-            alert("Identifiant incorrect")
+$(window).ready(
+    function () {
+        var private = $('#private')
+        var album = $('#album')
+        var pimg = $('#pimg')
+        var coms = $('#coms')
+        var titre = $('#bite')
+        var definition = $('#headcom')
+        var th2 = $('#th2')
+        var coms2 = $('#coms2')
+        var userObj
 
-        } else if (userlogin.length < 4) {
-            alert("Identifiant incorrect")
-        }
-
-    })
-    $('#inscription').submit(function (event) {
-        event.preventDefault()
-        var password = $('#passworde').val()
-        var pseudo = $('#pseudo').val()
-        if (password == "" || pseudo == "") {
-            alert("Champs pas remplis")
-        } else {
-            // le pseudo existe il ? et si oui le mdp correspond il ?
-            let pseudoExist = false
-            let x
-            for (x in userObj.users) {
-                let actualUser  = userObj.users[x]
-                if (actualUser.pseudo == pseudo) {
-                    pseudoExist = true
-                    break;
-                }
-            }
-            if (pseudoExist) {
-                alert(Pseudo déjà existant)
+        album.hide()
+        $('#inscription').submit(function (event) {
+            event.preventDefault()
+            var password = $('#passworde').val()
+            var pseudo = $('#pseudo').val()
+            if (password == "" || pseudo == "") {
+                alert("Champs pas remplis")
             } else {
-                //Etape : créateur de l'objet utilisateur :
-                var user = {
-                    id: uuidv4(),
-                    pseudo: pseudo,
-                    mdp: mdp,
+                // le pseudo existe il ? et si oui le mdp correspond il ?
+                let pseudoExist = false
+                let x
+                for (x in userObj.users) {
+                    let actualUser = userObj.users[x]
+                    if (actualUser.pseudo == pseudo) {
+                        pseudoExist = true
+                        break;
+                    }
                 }
-                //Etape d : Ajout du nouvel utilisateur dans la liste des utilisateurs
-                usersObj.users.push(user);
-                //Etape e : Sauvegarde dans le localStorage
-                saveUsers()
+                if (pseudoExist) {
+                    alert("Pseudo déjà existant")
+                } else {
+                    //Etape : créateur de l'objet utilisateur :
+                    var user = {
+                        id: uuidv4(),
+                        pseudo: pseudo,
+                        mdp: mdp,
+                    }
+                    //Etape d : Ajout du nouvel utilisateur dans la liste des utilisateurs
+                    usersObj.users.push(user);
+                    //Etape e : Sauvegarde dans le localStorage
+                    saveUsers()
 
-                //ETAPES FACULTATIVES :
-                //On va vider les champs :
-                $('#registerPseudo').val("")
-                $('#registerPassword').val("")
-                //on va afficher le panneau de login
-                displayLogin()
+                    //ETAPES FACULTATIVES :
+                    //On va vider les champs :
+                    $('#registerPseudo').val("")
+                    $('#registerPassword').val("")
+                    //on va afficher le panneau de login
+                    displayLogin()
+                }
             }
-                }
 
+        })
+        $('#thdr').click(function (event) {
+            titre.text("HDR")
+            definition.text("HDR c'est marrant")
+        })
+        $('#hklk').click(function (event) {
+            titre.text("High Key / Low Key")
+            definition.text("Contraste de couleur oh oh")
+        })
+        $('#hs').click(function (event) {
+            titre.text("High Speed")
+            definition.text("Equivalent de la drogue")
+        })
+        $('#studio').click(function (event) {
+            titre.text("Studio")
+            definition.text("Lieu ou tu peux vivre")
+        })
+        $('#pl').click(function (event) {
+            titre.text("Pose Longue")
+            definition.text("Une pose s'impause")
+        })
+        $('#ts').click(function (event) {
+            titre.text("Tilt Shift")
+            definition.text("Tilte Shifteux")
+        })
+        $('#lp').click(function (event) {
+            titre.text("Light Painting")
+            definition.text("Peinture claire")
+        })
+        $('#nb').click(function (event) {
+            titre.text("Noir et Blanc")
+            definition.text("Mélange de couleur oh oh")
+        })
+        $('#portrait').click(function (event) {
+            titre.text("Portrait")
+            definition.text("Visage de profil")
+        })
+        $('#paysage').click(function (event) {
+            titre.text("Paysage")
+            definition.text("Pas de profil")
+        })
+        $('#architecture').click(function (event) {
+            titre.text("Architecture")
+            definition.text("Batiment")
+        })
+        $('#graphisme').click(function (event) {
+            titre.text("Graphisme")
+            definition.text("Gramisme")
+        })
+        $('#evenements').click(function (event) {
+            titre.text("Evenements")
+            definition.text("Mariage ?")
+        })
+        $('#private').click(function (event) {
+            titre.text("Boudoire")
+            definition.text("Non")
+
+        })
+        $('#animo, #saison, #fetes, #nature, #urbain, #sport, #nuit').click(function(event){
+            if (album.hide()) {
+                album.show()
+                pimg.hide()
+            }
+            if (coms.show()){
+                coms.hide()
+            }
+            coms.removeClass("rightcolumn")
+            $('#album').addClass("i2")
+            $('#album').css("width", "100%")
+
+
+        })
+        $('#thdr, #hklk, #hs, #studio, #pl, #ts, #lp, #nb, #portrait, #paysage, #architecture, #graphisme, #evenements, #private').click(function(event){
+            if (album.show()){
+                album.hide()
+                pimg.show()
+            }
+        })
+        $('#animo').click(function(event){
+            titre.text("")
+            th2.text('Animaux')
+            definition.text("Quel belle animal")
+        })
+        $('#saison').click(function(event){
+            titre.text("")
+            definition.text("Automne / Eté / Hiver / Printemps")
+        })
+        $('#fetes').click(function(event){
+            titre.text("")
+            definition.text("Bar-mitzvah ?")
+        })
+        $('#nature').click(function(event){
+            titre.text("Nature")
+            definition.text("La nature dans toute sa splendeur")
+        })
+        $('#urbain').click(function(event){
+            titre.text("Urbain")
+            definition.text("Street Art")
+        })
+        $('#sport').click(function(event){
+            titre.text("Sport")
+            definition.text("Shaq'in fool")
+        })
+        $('#nuit').click(function(event){
+            titre.text("Nuit")
+            definition.text("C'est joli même de nuit")
+        })
     })
-})
