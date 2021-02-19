d2b43bf2fc6b94c9d789c9a9ab4f7147f64c20f9
maxence.schepens <maxence.schepens@gmail.com>
2021-02-19 09:25:22 +0100

    Tchoin

--- moutmoutmout.js
+++ moutmoutmout.js
@@ -9,7 +9,7 @@ $(window).ready(function () {
     var definition1 = $('#headcom1')
     var th2 = $('#th2')
     var coms2 = $('#coms2')
-   
+
 
 
     album.hide()
@@ -230,3 +230,31 @@ $('#btnProfil').click(() => {
     $('#form').hide()
     $('#biographie').show()
 })
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
